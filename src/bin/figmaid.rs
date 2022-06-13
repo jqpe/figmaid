a1a4be54fdbaeeef071434e14630b5a46d6454ae
Jasper Nykänen <nykanen@hey.com>
2022-06-13 16:43:25 +0300

    feat: remove figmaid-validate and move to single binary

--- src/bin/figmaid.rs
+++ src/bin/figmaid.rs
@@ -22,14 +22,7 @@ async fn main() {
     let args = Args::parse();
 
     if args.t {
-        let output = std::process::Command::new("figmaid-validate")
-            .output()
-            .expect("figmaid-validate is not installed");
-
-        io::stdout().write_all(&output.stdout).unwrap();
-        io::stderr().write_all(&output.stderr).unwrap();
-
-        return;
+        return validate().await;
     }
 
     let config = load_config();
@@ -46,5 +39,67 @@ async fn main() {
             }
         }
         Err(err) => panic!("{:?}", err),
+    }
+}
+
+/// Validates the configuration file and optionally creates the default configuration if it doesn't exist.
+async fn validate() {
+    use std::{io::stdout, path::Path};
+
+    use figmaid::config::{is_config_valid, Config};
+    use opener::open;
+
+    let config_path = Path::new(&home::home_dir().expect("Couldn't get home directory"))
+        .join(".config/figmaid/figmaid.json");
+
+    match std::fs::read(config_path.clone()) {
+        Ok(config) => {
+            if let Ok(config) = serde_json::from_str(&String::from_utf8_lossy(&config)) {
+                if is_config_valid(&config).await {
+                    println!("Configuration is OK.")
+                }
+            } else {
+                println!("Configuration is not valid JSON.")
+            }
+        }
+        _ => {
+            let mut choice = String::new();
+
+            eprint!("Couldn't load configuration file, create it y/N? ");
+            stdout()
+                .flush()
+                .unwrap_or_else(|err| eprintln!("Couldn't flush stdout: {err}"));
+
+            io::stdin()
+                .read_line(&mut choice)
+                .expect("Couldn't read line");
+
+            match choice.trim() {
+                "y" | "Y" => {
+                    match std::fs::write(config_path.clone(), Config::default().to_string()) {
+                        Ok(_) => {
+                            print!("Succesfully created configuration file, open it y/N? ");
+                            stdout()
+                                .flush()
+                                .unwrap_or_else(|err| eprintln!("Couldn't flush stdout: {err}"));
+
+                            let mut choice = String::new();
+
+                            io::stdin()
+                                .read_line(&mut choice)
+                                .expect("Couldn't read line");
+
+                            if "y" == choice.trim() {
+                                open(config_path).expect("Couldn't open");
+                            }
+                        }
+                        Err(err) => {
+                            eprintln!("Couldn't create configuration file: {}", err)
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 }
