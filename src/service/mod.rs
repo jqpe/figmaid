8eec68a1c0d85e00da2a6a02a71073248fb1c94f
Jasper Nykänen <nykanen@hey.com>
2022-06-14 09:37:35 +0300

    refactor: move route handlers to functions

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -17,7 +17,7 @@ use url::Url;
 
 use font_metadata::load_fonts;
 
-use crate::config::load_config;
+use crate::config::{load_config, Config};
 
 const FIGMA_FONT_FILES: &str = "/figma/font-files";
 const FIGMA_FONT_FILE: &str = "/figma/font-file";
@@ -32,59 +32,64 @@ pub async fn figma(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         .unwrap();
 
     match (req.uri().path(), req.method()) {
-        // Responds with a JSON of all font files
         (FIGMA_FONT_FILES, &Method::GET | &Method::OPTIONS) => {
-            let font_files: serde_json::Value = load_fonts(config.directories).into();
-            let json = json!({
-                "version": 4i8,
-                "fontFiles": font_files,
-            });
-
-            response
-                .headers_mut()
-                .insert("content-type", HeaderValue::from_static("application/json"));
-            *response.body_mut() = Body::from(json.to_string());
+            handle_font_files(config, &mut response)
         }
-        (FIGMA_FONT_FILE, &Method::GET | &Method::OPTIONS) => {
-            if let Ok(url) = Url::parse(
-                ["http://localhost", req.uri().to_string().as_str()]
-                    .join("")
-                    .as_str(),
-            ) {
-                let file = url.query_pairs().find(|(key, _)| key == "file");
-
-                if let Some((_, file)) = file {
-                    if let Ok(font_file) = fs::read(&*file) {
-                        let re = Regex::new(r"(otf|ttc|ttf)$").unwrap();
-                        if let Some(ext) = re.find(&file) {
-                            match ext.as_str() {
-                                "ttf" | "ttc" => {
-                                    response.headers_mut().insert(
-                                        "content-type",
-                                        HeaderValue::from_static("application/x-font-ttf"),
-                                    );
-                                }
-                                "otf" => {
-                                    response.headers_mut().insert(
-                                        "content-type",
-                                        HeaderValue::from_static("font/otf"),
-                                    );
-                                }
-                                _ => {}
-                            };
-                        };
-
-                        *response.body_mut() = Body::from(Bytes::from(font_file));
-                    }
-                }
-            } else {
-                *response.status_mut() = StatusCode::NOT_FOUND;
-            }
-        }
+        (FIGMA_FONT_FILE, &Method::GET | &Method::OPTIONS) => handle_font_file(req, &mut response),
         _ => {
             *response.status_mut() = StatusCode::BAD_REQUEST;
         }
     };
 
     Ok(response)
+}
+
+/// Responds with a single font file with headers for otf/ttf/ttc
+fn handle_font_file(req: Request<Body>, response: &mut Response<Body>) {
+    if let Ok(url) = Url::parse(
+        ["http://localhost", req.uri().to_string().as_str()]
+            .join("")
+            .as_str(),
+    ) {
+        let file = url.query_pairs().find(|(key, _)| key == "file");
+
+        if let Some((_, file)) = file {
+            if let Ok(font_file) = fs::read(&*file) {
+                let re = Regex::new(r"(otf|ttc|ttf)$").unwrap();
+                if let Some(ext) = re.find(&file) {
+                    match ext.as_str() {
+                        "ttf" | "ttc" => {
+                            response.headers_mut().insert(
+                                "content-type",
+                                HeaderValue::from_static("application/x-font-ttf"),
+                            );
+                        }
+                        "otf" => {
+                            response
+                                .headers_mut()
+                                .insert("content-type", HeaderValue::from_static("font/otf"));
+                        }
+                        _ => {}
+                    };
+                };
+
+                *response.body_mut() = Body::from(Bytes::from(font_file));
+            }
+        }
+    } else {
+        *response.status_mut() = StatusCode::NOT_FOUND;
+    }
+}
+
+/// Responds with a JSON of all font files
+fn handle_font_files(config: Config, response: &mut Response<Body>) {
+    let font_files: serde_json::Value = load_fonts(config.directories).into();
+    let json = json!({
+        "version": 4i8,
+        "fontFiles": font_files,
+    });
+    response
+        .headers_mut()
+        .insert("content-type", HeaderValue::from_static("application/json"));
+    *response.body_mut() = Body::from(json.to_string());
 }
