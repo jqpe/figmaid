36a2fa433ca4ff4c37305ccfb0dc2852d0b184a6
Jasper Nykänen <nykanen@hey.com>
2022-06-17 14:00:18 +0300

    feat: use localhost

--- src/server.rs
+++ src/server.rs
@@ -3,14 +3,14 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::Server;
 use std::convert::Infallible;
-use std::net::SocketAddr;
+use std::net::{Ipv4Addr, SocketAddr};
 
 use crate::config::load_config;
 use crate::service::figma;
 
 pub async fn start_server() {
     let config = load_config();
-    let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
+    let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, config.port));
     let figma = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(figma)) });
     let server = Server::try_bind(&addr);
 
